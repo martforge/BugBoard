fc5b09f4fb91fff29818d40245e07147919929e1
martforge <martforge@gmail.com>
2026-09-01 05:52:59 +0800

    Add login and registration validation

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,21 +1,84 @@
 console.log("BugBoard JavaScript loaded");
 
-const loginForm = document.getElementById("loginForm")
+const loginForm = document.getElementById("loginForm");
 console.log(loginForm)
-
-loginForm.addEventListener("submit",function(event){
+//Login
+if (loginForm){
+    loginForm.addEventListener("submit",function(event){
     event.preventDefault();
-    
+
+    const loginMessage = document.getElementById("loginMessage");
+    loginMessage.textContent = "";
     const usernameInput = document.getElementById("username")
-    
-    console.log(`Username: ${usernameInput.value}`);
-    console.log("Password: Password received");
-    
+        if (usernameInput.value === "") {
+            loginMessage.textContent = "Username is required";
+            return
+        }
+        else {loginMessage.textContent = "Username accepted";
+        }
+    const savedUsername = localStorage.getItem("username");
+    console.log(savedUsername);
+    if (savedUsername !== usernameInput.value ) {
+    console.log("Username does not match")
+    return;
+    }
+
+    //password
+    const passwordInput = document.getElementById("password");
+        if (passwordInput.value === "") {
+            loginMessage.textContent = "Password is required";
+            return;
+        }
+
+        else {loginMessage.textContent = "Password accepted";
+        }
+    loginMessage.textContent = "Login successful";
     console.log("Login form submitted")
     });
+}
+
+// Register
+const registerForm = document.getElementById("registerForm");
+// Register Validation
+if (registerForm){
+    registerForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const registerMessage = document.getElementById("registerMessage");
+    registerMessage.textContent = "";
+        // Username
+        const usernameInput = document.getElementById("username");
+        if (usernameInput.value === ""){
+            registerMessage.textContent = "Username is required"
+            return;
+        }
+
+        //Email
+        const emailInput = document.getElementById("email");
+        if (emailInput.value === ""){
+            registerMessage.textContent = "Email is required";
+            return;
+        }
+        // Password
+        const passwordInput = document.getElementById("password");
+        if (passwordInput.value === ""){
+            registerMessage.textContent = "Password is required";
+            return;
+        }
+        registerMessage.textContent = "Registration successful";
+        console.log("Registration successful");
+        localStorage.setItem("username", usernameInput.value);
+        console.log(localStorage.getItem("username"));
 
 
 
+
+
+
+
+
+
+    });
+}
 
 
 
